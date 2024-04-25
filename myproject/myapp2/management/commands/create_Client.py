16611b044d13e0e582c27a4bd7d28b7635677636
xokc4 <42719177+xokc4@users.noreply.github.com>
2024-04-25 18:20:03 +0400

    s

--- myproject/myapp2/management/commands/create_Client.py
+++ myproject/myapp2/management/commands/create_Client.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from myproject.myapp2.models import Client
+from myapp2.models import Client
 
 class Command(BaseCommand):
     help = "Create user."
